2596acefffe6699b13eb9beb1d34b089553022ad
Mani Sagar Pratha <sagarcourse@gmail.com>
2017-12-05 02:23:18 +0530

    Add files via upload
    Draft version

---
+++ CtrlCP/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace CtrlCP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSource = new System.Windows.Forms.Button();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtTarget = new System.Windows.Forms.TextBox();
+            this.btnTarget = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.FileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.FolderBrowse = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // btnSource
+            // 
+            this.btnSource.Location = new System.Drawing.Point(45, 53);
+            this.btnSource.Name = "btnSource";
+            this.btnSource.Size = new System.Drawing.Size(75, 23);
+            this.btnSource.TabIndex = 0;
+            this.btnSource.Text = "Source";
+            this.btnSource.UseVisualStyleBackColor = true;
+            this.btnSource.Click += new System.EventHandler(this.btnSource_Click);
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(150, 53);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(381, 20);
+            this.txtSource.TabIndex = 1;
+            // 
+            // txtTarget
+            // 
+            this.txtTarget.Location = new System.Drawing.Point(150, 108);
+            this.txtTarget.Name = "txtTarget";
+            this.txtTarget.Size = new System.Drawing.Size(381, 20);
+            this.txtTarget.TabIndex = 2;
+            // 
+            // btnTarget
+            // 
+            this.btnTarget.Location = new System.Drawing.Point(45, 108);
+            this.btnTarget.Name = "btnTarget";
+            this.btnTarget.Size = new System.Drawing.Size(75, 23);
+            this.btnTarget.TabIndex = 3;
+            this.btnTarget.Text = "Target";
+            this.btnTarget.UseVisualStyleBackColor = true;
+            this.btnTarget.Click += new System.EventHandler(this.btnTarget_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(456, 166);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 4;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // FileDialog
+            // 
+            this.FileDialog.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(584, 243);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnTarget);
+            this.Controls.Add(this.txtTarget);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.btnSource);
+            this.Name = "Form1";
+            this.Text = "Control C + V";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSource;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtTarget;
+        private System.Windows.Forms.Button btnTarget;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.OpenFileDialog FileDialog;
+        private System.Windows.Forms.FolderBrowserDialog FolderBrowse;
+    }
+}
+
---
+++ CtrlCP/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using Microsoft.Office.Core;
+using Excel =Microsoft.Office.Interop.Excel;
+namespace CtrlCP
+{
+    public partial class Form1 : Form
+    {
+        string sourcepath="";
+        string targetlocation = "";
+        string filename = "";
+        public Form1()
+        {
+            InitializeComponent();
+            txtSource.ReadOnly = true;
+            txtTarget.ReadOnly = true;
+
+        }
+
+        private void btnSource_Click(object sender, EventArgs e)
+        {
+            DialogResult result = FileDialog.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                //Source File Name
+                filename = FileDialog.FileName.Split('\\').Last();
+                //Source full path
+                sourcepath = FileDialog.FileName;
+                //Displaying the source path in the text box
+                txtSource.Paste(sourcepath);
+            }
+        }
+
+        private void btnTarget_Click(object sender, EventArgs e)
+        {
+            DialogResult result = FolderBrowse.ShowDialog(); 
+            if (result == DialogResult.OK) 
+            {
+                //Destination path
+                targetlocation = FolderBrowse.SelectedPath;
+                //Displaying the source path in the text box
+                txtTarget.Paste(targetlocation);
+
+            }
+
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            FileInfo srcfileinfo = new FileInfo(sourcepath);
+            srcfileinfo.CopyTo(targetlocation+"\\"+filename,false);
+            Microsoft.Office.Interop.Excel.Application oXL;
+            Microsoft.Office.Interop.Excel._Workbook oWB;
+            Microsoft.Office.Interop.Excel._Worksheet oSheet;
+            Microsoft.Office.Interop.Excel.Range oRng;
+            object misvalue = System.Reflection.Missing.Value;
+
+            //Start Excel and get Application object.
+            oXL = new Microsoft.Office.Interop.Excel.Application();
+            oXL.Visible = true;
+
+            //Get a new workbook.
+            oWB = (Microsoft.Office.Interop.Excel._Workbook)(oXL.Workbooks.Add(""));
+            oSheet = (Microsoft.Office.Interop.Excel._Worksheet)oWB.ActiveSheet;
+
+            //Add table headers going cell by cell.
+            oSheet.Cells[1, 1] = "Source File Name";
+            oSheet.Cells[1, 2] = "Source Path";
+            oSheet.Cells[1, 3] = "Destination Path";
+
+            //Format A1:C1 as bold, vertical alignment = center.
+            oSheet.get_Range("A1", "C1").Font.Bold = true;
+            oSheet.get_Range("A1", "C1").VerticalAlignment =
+                Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
+
+            // Create an array to multiple values at once.
+            string[] values = new string[] { filename, sourcepath, targetlocation };
+            //Fill A2:C2 with an array of values
+            oSheet.get_Range("A2", "C2").Value2 = values;
+
+
+            //AutoFit columns A:C.
+            oRng = oSheet.get_Range("A1", "C1");
+            oRng.EntireColumn.AutoFit();
+
+            oXL.Visible = false;
+            oXL.UserControl = false;
+            oWB.SaveAs("d:\\CopyPaste.xls", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing,
+                false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+            oWB.Close();
+            MessageBox.Show("Excel file created , you can find the file d:\\CopyPaste.xls");
+
+
+
+
+        }
+
+    }
+}
